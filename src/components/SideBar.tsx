c045784cba47ab51b6a5bd974b6b6e33e26d2eab
Willian Git <57255222+WrsDeveloper@users.noreply.github.com>
2021-10-14 11:03:17 -0300

    implements soluction

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from "../App";
+import { Button } from "./Button";
+
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  onClick: (genreId: number) => void;
+  selected: number;
 }
+export const SideBar: React.FC<SideBarProps> = ({
+  genres,
+  onClick,
+  selected,
+}) => {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onClick(genre.id)}
+            selected={selected === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+};
